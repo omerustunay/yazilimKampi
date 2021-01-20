d5c14ea50d0ea10ee51f7bba1c1246b5a742a927
Omer Ustunay <35745606+omerustunay@users.noreply.github.com>
2021-01-20 20:09:11 +0300

    odev-1 projesi yüklendi.

---
+++ ClassMetotDemo/ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> musteriListesi = new List<Musteri>();
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi: "+ musteri.Adi);
+           
+            
+
+            musteriListesi.Add(musteri);
+            
+        }
+        public void Listele()
+        {
+            Console.WriteLine("musteri listesi : ");
+            foreach(Musteri m in musteriListesi)
+            {
+                Console.WriteLine("musteri adi: "+ m.Adi 
+                    +" musteri soyadi : "+ m.Soyadi 
+                    +" musteri meslegi : "+m.Meslek);
+            }
+        }
+
+        public void Sil(int Id)
+        {
+            musteriListesi.RemoveAt(Id);
+        }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("baslayalim");
+
+            Musteri m1 = new Musteri();
+            m1.Adi = "john";
+            m1.Soyadi = "wick";
+            m1.Meslek = "muhendis";
+
+
+            Musteri m2 = new Musteri();
+            m2.Adi = "clara";
+            m2.Soyadi = "cena";
+            m2.Meslek = "mimar";
+            
+            Musteri m3 = new Musteri();
+            m3.Adi = "edison";
+            m3.Soyadi = "karl";
+            m3.Meslek = "doktor";
+
+
+            MusteriManager manager = new MusteriManager();
+            manager.Ekle(m1);
+            manager.Ekle(m2);
+            manager.Ekle(m3);
+
+            manager.Listele();
+
+            manager.Sil(1);
+
+            manager.Listele();
+        }
+    }
+}
