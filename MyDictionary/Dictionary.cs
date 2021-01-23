fe468308577284c107aa3d027c50ee49866b52f3
Omer Ustunay <35745606+omerustunay@users.noreply.github.com>
2021-01-23 21:01:55 +0300

    odev-2 dictionary kavramı projesi yüklendi.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<T1, T2>
+    {
+        T1[] array1;
+        T2[] array2;
+        public Dictionary()
+        {
+            array1 = new T1[0];
+            array2 = new T2[0];
+        }
+
+        public void Add(T1 key1, T2 key2)
+        {
+            T1[] tempKeys1 = array1;
+            T2[] tempKeys2 = array2;
+            array1 = new T1[tempKeys1.Length + 1];
+            array2 = new T2[tempKeys2.Length + 1];
+
+            for (int i = 0; i < tempKeys1.Length; i++)
+            {
+                array1[i] = tempKeys1[i];
+                array2[i] = tempKeys2[i];
+            }
+            array1[tempKeys1.Length] = key1;
+            array2[tempKeys2.Length] = key2;
+        }
+        public void Write()
+        {
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(array2[i] + ":" + array1[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+            myDictionary.Add("Blue", "Mavi");
+            myDictionary.Add("Yellow", "Sarı");
+            myDictionary.Add("Green", "Yeşil");
+
+            myDictionary.Write();
+            
+        }
+
+        
+    }
+}
