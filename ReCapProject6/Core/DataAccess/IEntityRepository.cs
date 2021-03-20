8a80ee74ea861a20a16c08634583442d2a81c507
omerustunay <omerustunay@omerustunay.local>
2021-03-21 02:45:46 +0300

    12.gun odevleri yapıldı.

---
+++ ReCapProject6/Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Core.DataAccess.Entities;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T>where T:class,IEntity,new ()
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+    }
+}
