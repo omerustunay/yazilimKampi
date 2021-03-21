dee26413bd5c75276449351e2a54c7bef01a7f0d
omerustunay <omerustunay@omerustunay.local>
2021-03-22 02:16:19 +0300

    13.gun 1.odev yuklendi.

---
+++ ReCapProject7/DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System.Linq;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCustomerDal : EfEntityRepositoryBase<Customer, RecapDbContext>, ICustomerDal
+    {
+        public List<CustomerDetailDto> GetCustomerDetails()
+        {
+            using (RecapDbContext context = new RecapDbContext())
+            {
+                var result = from c in context.Customers
+                             join u in context.Users
+                             on c.UserId equals u.Id
+                             select new CustomerDetailDto
+                             {
+                                 UserFirstName = u.FirstName,
+                                 UserLastName = u.LastName,
+                                 CompanyName = c.CompanyName
+                             };
+                return result.ToList();
+            }
+        }
+    }
+}
---
+++ ReCapProject7/DataAccess/Concrete/EntityFramework/RecapDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class RecapDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ReCapProjectDb;Trusted_Connection=true");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CarImage> CarImages { get; set; }
+    }
+}
