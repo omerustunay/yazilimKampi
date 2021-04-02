13e095cf2685448ee774354b822d16fa19c12fe1
omerustunay <omerustunay@omerustunay.local>
2021-04-03 00:06:01 +0300

    14. gun 2. odev

---
+++ ReCapProject8/DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, RecapDbContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (RecapDbContext context = new RecapDbContext())
+            {
+                var result = (from car in context.Cars
+                             join c in context.Colors on car.ColorId equals c.Id
+                             join b in context.Brands on car.BrandId equals b.Id
+                             select new CarDetailDto
+                             {
+                                 DailyPrice = car.DailyPrice,
+                                 ModelYear = car.ModelYear,
+                                 ColorName = c.ColorName,
+                                 BrandName = b.BrandName,
+                                 Description = car.Description
+                             });
+
+                return result.ToList();
+            }
+        }
+    }
+}
---
+++ ReCapProject8/DataAccess/Concrete/EntityFramework/EfCarImageDal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System.Linq;
+using DataAccess.Concrete.EntityFramework.Contexts;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarImageDal : EfEntityRepositoryBase<CarImage, RecapDbContext>, ICarImageDal
+    {
+        public List<CarImageDetailDto> GetCarImageDetails()
+        {
+            using (RecapDbContext context = new RecapDbContext())
+            {
+                var result = from c in context.CarImages
+                             join car in context.Cars on c.CarId equals car.Id
+                             join b in context.Brands on car.BrandId equals b.Id
+                             join co in context.Colors on car.ColorId equals co.Id
+                             select new CarImageDetailDto
+                             {
+                                 BrandName = b.BrandName,
+                                 CarName = car.Description,
+                                 ColorName = co.ColorName,
+                                 DailyPrice = car.DailyPrice,
+                                 Date = c.Date,
+                                 ImagePath = c.ImagePath,
+                                 ModelYear = car.ModelYear
+                             };
+                return result.ToList();
+            }
+        }
+    }
+}
